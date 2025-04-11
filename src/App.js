fd7648510de002d1d67d8c8340499c192105f7d1
Riadh98 <riadhpharma98@gmail.com>
2025-04-11 21:51:48 +0200

    Ajout du texte d'accueil

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: 'center', paddingTop: '50px' }}>
+      <h1>Bienvenue sur CV Designer Pro 🚀</h1>
+      <p>Ton outil en ligne pour créer un CV stylé, clair et professionnel !</p>
     </div>
   );
 }
